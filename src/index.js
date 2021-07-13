a41d8893f3f961b65396ca24ddd665f979292fc7
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-07-13 23:12:19 +0100

    Add form eventlistener to index.js

--- src/index.js
+++ src/index.js
@@ -8,4 +8,30 @@ const todoInput = document.querySelector("#todoInput");
 const todoList = document.querySelector("#todoList");
 const clearButton = document.querySelector("#clearBtn");
 
+document.addEventListener("DOMContentLoaded", () => {
+    form.addEventListener("submit", (e) => {
 
+      e.preventDefault();
+
+      const todo = todoInput.value.trim();
+      if (todo.length === 0) {
+        showAlert("Add to your list...", "alert-danger");
+        return;
+      } else {
+          // Add new Item
+          const itemObj = {
+            name: todo
+          };
+          todoItems.push(itemObj);
+          // set local storage
+          setLocalStorage(todoItems);
+          showAlert("New item has been added.", "alert-success");
+        }
+  
+        getList(todoItems);
+        // get list of all items
+      }
+      console.log(todoItems);
+      todoInput.value = "";
+    });
+  
