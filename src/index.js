c1dae3d3255c53735e206787934e23cd3c2a5247
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-07-17 12:28:59 +0100

    Fix drag up bug

--- src/index.js
+++ src/index.js
@@ -5,5 +5,41 @@ const todoList = document.querySelector('.todoList');
 
 const items = document.querySelectorAll('.item');
 
+const list = [
+  {
+    index: 1,
+    description: 'brush my teeth',
+    completed: true,
+  },
+  {
+    index: 2,
+    description: 'Go to school',
+    completed: false,
+  },
+  {
+    index: 3,
+    description: 'Sleep',
+    completed: false,
+  },
+];
+
+const displayList = (list) => {
+  list.forEach((e) => {
+    const lists = document.createElement('div');
+    lists.classList.add('mini-section');
+    lists.classList.add('item');
+    lists.setAttribute('draggable', 'true');
+    lists.innerHTML = `
+      <span>
+      <input class="check" type="checkbox" id="task-description" name="task-description" value="${e.index}">${e.description}
+      <i class="fas fa-ellipsis-v" style="color: gray; float: right;"></i>
+      </span>
+      `;
+    todoList.appendChild(lists);
+  });
+};
+
+displayList(list);
+
 addListeners(items);
 dragOver(todoList);
--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  devServer: {
-    contentBase: './dist',
-  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 8080,
+    open: true,
+  },
+
   module: {
     rules: [
       {
